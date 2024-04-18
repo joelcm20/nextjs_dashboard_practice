ba32575703b56118ca690fcd2bc0e11bd7ae65d2
joelcm20 <cjoel1977@gmail.com>
2024-04-18 17:41:06 -0300

    layout

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { subscribe } from 'diagnostics_channel';
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: '400' });
