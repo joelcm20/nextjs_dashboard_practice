53da2e41b270c8c40a3a16b79ff6c74f20620c69
joelcm20 <cjoel1977@gmail.com>
2024-04-28 12:31:59 -0300

    handling errors

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
@@ -8,6 +9,11 @@ export default async function Page({ params }: { params: { id: string } }) {
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
+
+  // set status 404 if invoice not exists
+  if (!invoice) {
+    notFound(); // this works only in server components
+  }
 
   return (
     <main>
